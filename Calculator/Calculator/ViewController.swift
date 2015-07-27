0c50e511bc5d73800eea980defd1e1fd128fdb97
2hf <zh.f@outlook.com>
2015-07-27 23:15:49 +0800

    projects for practice

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Fan Zhang on 15/3/15.
+//  Copyright (c) 2015年 ZH.F. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        println("digit = \(digit)")
+    }
+
+}
+
